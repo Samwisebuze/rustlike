bf4b13640a61f41834b6baecf9ea6cdc41e184f2
Samuel Buzas <buzassg@mail.uc.edu>
2020-01-12 17:27:06 -0500

    feat: Add Movement
    Added a basic left Movement system

--- src/main.rs
+++ src/main.rs
@@ -26,13 +26,50 @@ struct Renderable {
 //     type Storage = VecStorage<Self>;
 // }
 
+#[derive(Component)]
+struct LeftMover {}
+
+// An empty struct used to attach the logic in
+// the LeftWalker System below
+struct LeftWalker {}
+
+// Implement a specs System for the Left Mover Functionality
+// 'a is a lifetime specifier, so the system will 
+// exist just long enough to run.
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>, 
+                        WriteStorage<'a, Position>);
+    // Implement the run trait from System
+    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 { pos.x = 79; }
+        }
+    }
+}
+
 struct State {
-    ecs: World
+    ecs: World,
+
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        // Create a new mutable LeftWalker System
+        let mut lw = LeftWalker{};
+        // Run the Left walker system on the World
+        lw.run_now(&self.ecs);
+        // Tells the world to run any queued changes
+        self.ecs.maintain();
+    }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
+
+        self.run_systems();
+        
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
@@ -50,6 +87,7 @@ fn main() {
     // Register Components with the GameState
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
 
     // Create a new Entity
     gs.ecs
@@ -72,6 +110,7 @@ fn main() {
             fg: RGB::named(rltk::RED),
             bg: RGB::named(rltk::BLACK),
         })
+        .with(LeftMover{})
         .build();
     }
 
